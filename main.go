fb008d9a8aecf44c21859015bc0db3fe68de5f6c
Eduardo Ismael <eduardo78d>
2016-12-29 15:50:46 -0600

    layout

--- main.go
+++ main.go
@@ -12,9 +12,15 @@ func index(w http.ResponseWriter, r *http.Request){
 	template.RenderTemplate(w, r, "index/home", nil)
 }
 
+func register(w http.ResponseWriter, r *http.Request){
+	template.RenderTemplate(w, r, "user/register", nil)
+}
+
 func main(){
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", index)
+	mux.HandleFunc("/register", register)
+
 	mux.Handle("/assets/",  http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))), )
 
 	log.Println("Server listening in 127.0.0.1"+ port)
--- template/template.go
+++ template/template.go
@@ -2,20 +2,45 @@ package template
 
 import(
 	"html/template"
-	"fmt"
 	"net/http"
+	"fmt"
+	"bytes"
+)
+
+var layoutFuncs = template.FuncMap{
+	"yield": func() (string, error){
+		return "", fmt.Errorf("Yield called innappropriatley")
+	},
+}
+
+var layout = template.Must(
+	template.New("layout.html").Funcs(layoutFuncs).ParseFiles("templates/layout.html"),
 )
 
 var templates = template.Must(template.New("t").ParseGlob("./templates/**/*.html"))
-var errorTemplate = "<html><body><h2>Error rendering template!</h2> </body></html>"
 
 func RenderTemplate(w http.ResponseWriter, r *http.Request, name string, data interface{}){
+	funcs := template.FuncMap{
+		"yield": func() (template.HTML, error){
+			buf := bytes.NewBuffer(nil)
+			err := templates.ExecuteTemplate(buf, name, data)
+			return template.HTML(buf.String()) , err
+		},
+	}
+
+	layoutClone, _ := layout.Clone()
+	layoutClone.Funcs(funcs)
+	err := layoutClone.Execute(w, data)
+
+	if err != nil{
+		http.Error( w, "Page not found!", http.StatusInternalServerError, )
+	}
+}
+
+
+func LastRenderTemplate(w http.ResponseWriter, r *http.Request, name string, data interface{}){
 	err := templates.ExecuteTemplate(w, name, data)
 	if err != nil {
-		http.Error(
-			w,
-			fmt.Sprintf(errorTemplate, name, err),
-			http.StatusInternalServerError,
-		)
+		http.Error( w, "Page not found!", http.StatusInternalServerError, )
 	}
 }
